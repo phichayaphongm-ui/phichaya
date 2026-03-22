0c696032c95839ad09944ae13e25cabaf5dedb05
phichayaphongm-ui <phichayaphong.m@gmail.com>
2026-03-22 15:17:33 +0700

    chore: allow cloudflare insights script in csp

--- next.config.mjs
+++ next.config.mjs
@@ -19,7 +19,7 @@ const nextConfig = {
         headers: [
           {
             key: 'Content-Security-Policy',
-            value: "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' blob: https://*.youtube.com https://*.ytimg.com https://*.google.com https://*.gstatic.com https://*.googletagmanager.com https://*.google-analytics.com https://*.doubleclick.net https://*.googleapis.com https://*.google-events.com https://tag.g.doubleclick.net; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; img-src 'self' data: https://*.ytimg.com https://*.google.com https://*.vercel.app; font-src 'self' https://fonts.gstatic.com; frame-src 'self' https://*.youtube.com https://youtube.com; worker-src 'self' blob:; connect-src 'self' https://*.google-analytics.com;"
+            value: "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' blob: https://*.youtube.com https://*.ytimg.com https://*.google.com https://*.gstatic.com https://*.googletagmanager.com https://*.google-analytics.com https://*.doubleclick.net https://*.googleapis.com https://*.google-events.com https://tag.g.doubleclick.net https://static.cloudflareinsights.com; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; img-src 'self' data: https://*.ytimg.com https://*.google.com https://*.vercel.app; font-src 'self' https://fonts.gstatic.com; frame-src 'self' https://*.youtube.com https://youtube.com; worker-src 'self' blob:; connect-src 'self' https://*.google-analytics.com https://cloudflareinsights.com;"
           },
           {
             key: 'X-Robots-Tag',
